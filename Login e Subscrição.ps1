fdf6ae224dc43a637f7c75152ac69488fccf958c
danielsrosa <danielsrosa@hotmail.com>
2017-05-11 20:13:38 -0300

    Versão Inicial
    Comandos de Login e Validação de Subscrição

---
+++ Login e Subscrição.ps1
@@ -0,0 +1,23 @@
+﻿
+
+Get-AzureRmSubscription
+
+Select-AzureRmSubscription -SubscriptionId "7cf2bdc1-d780-41d5-9d32-2f81017514ec"
+
+Get-AzureRmStorageAccount
+
+Get-AzureRmResourceGroup
+
+Login-AzureAsAccount
+
+
+Add-AzureAccount
+
+	
+Install-Module AzureRM
+Login-AzureRmAccount
+
+
+Get-Module -ListAvailable *Azure*Add
+
+
