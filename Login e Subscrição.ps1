ceec0cb2d9952b02429aa8d43708ae05bb67a20f
danielsrosa <danielsrosa@hotmail.com>
2017-05-13 16:41:26 -0300

    Inserido os Módulos

--- Login e Subscrição.ps1
+++ Login e Subscrição.ps1
@@ -1,23 +1,49 @@
 ﻿
-
 Get-AzureRmSubscription
 
-Select-AzureRmSubscription -SubscriptionId "7cf2bdc1-d780-41d5-9d32-2f81017514ec"
+Select-AzureRmSubscription -SubscriptionId "f7370fac-681f-4506-b8a2-24624c65bc25"
 
-Get-AzureRmStorageAccount
 
-Get-AzureRmResourceGroup
 
+
+#Instalar os módulos do Azure
+Install-Module AzureRM
+
+
+#Validar módulos do Azure instalados
+Get-Module -ListAvailable *Azure*Add
+
+
+#Login No Azure
+Login-AzureRmAccount
+
+Add-AzureAccount
 Login-AzureAsAccount
 
 
-Add-AzureAccount
 
-#Comentário	
-Install-Module AzureRM
-Login-AzureRmAccount
+Set-ExecutionPolicy RemoteSigned
 
 
-Get-Module -ListAvailable *Azure*Add
 
+Import-Module D:\Modules\Choose-Location
+Import-Module D:\Modules\Choose-LocationARM
+Import-Module D:\Modules\Create-Names
+Import-Module D:\Modules\Create-SQLDB
+Import-Module D:\Modules\Create-Storage
+Import-Module D:\Modules\Create-VirtualNetwork
+Import-Module D:\Modules\Create-VM
+Import-Module D:\Modules\Create-VNetARM
+Import-Module D:\Modules\Disable-IEESC
+Import-Module D:\Modules\Install-AD
+Import-Module D:\Modules\Install-WinRMCertificateForVM
+Import-Module D:\Modules\Remove-SubsAndAccounts
+Import-Module D:\Modules\Reset-Azure
+Import-Module D:\Modules\Setup-Azure
+Import-Module D:\Modules\Show-Subscription
+Import-Module D:\Modules\Show-SubscriptionARM
+
+Setup-Azure
+
+Reset-Azure
 
